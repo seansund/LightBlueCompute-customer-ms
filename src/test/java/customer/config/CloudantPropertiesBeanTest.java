f1ef68273ff3b5ebda09d33a60981c909c043436
Sean Sundberg <seansund@Seans-MacBook-Pro.local>
2017-11-13 08:55:00 -0600

    Updated dependencies for SpringBoot for testing

--- src/test/java/customer/config/CloudantPropertiesBeanTest.java
+++ src/test/java/customer/config/CloudantPropertiesBeanTest.java
@@ -10,12 +10,12 @@ import java.net.URL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes={TestApplication.class})
+@SpringBootTest(classes={TestApplication.class})
 @ActiveProfiles("test")
 public class CloudantPropertiesBeanTest {
 	@Autowired
--- src/test/java/customer/config/JWTPropertiesBeanTest.java
+++ src/test/java/customer/config/JWTPropertiesBeanTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes={TestApplication.class})
+@SpringBootTest(classes={TestApplication.class})
 @ActiveProfiles("test")
 public class JWTPropertiesBeanTest {
 	
